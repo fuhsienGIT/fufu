e731a52581f62d45a1527d90c805a7d3fb2c2354
fuhsienGIT <fu.hsien.loong@student.mmu.edu.my>
2024-09-06 19:41:13 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from dash import Dash, html
 
+# Testing
+
 app = Dash(__name__)
 server = app.server
 
-app.layout = html.H1('Hello World')
+app.layout = html.H1('MCM7183 Exercise 3')
 
 if __name__ == '__main__':
     app.run(debug=True)
